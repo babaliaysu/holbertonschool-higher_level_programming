7ed0852b19c417b2915fdbdc7423f1759f40d7f5
babaliaysu <babali.aysu2007@gmail.com>
2026-02-12 23:16:02 +0000

    13

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,6 @@ def print_last_digit(number):
         last_digit = (number * -1) % 10
     else:
         last_digit = number % 10
-    
+
     print("{:d}".format(last_digit), end="")
     return last_digit
