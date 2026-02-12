f9031348ff1c698c63e6bad57862fa2fb01ed20d
babaliaysu <babali.aysu2007@gmail.com>
2026-02-12 22:06:42 +0000

    yeni1

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(99): 
-    print(f"{i:02d}", end=", ")
-print(f"{99:02d}")
+for i in range(100):
+    if i < 99:
+        print("{:02d}".format(i),end=", ")
+    else:
+        print("{:02d}".format(i))
