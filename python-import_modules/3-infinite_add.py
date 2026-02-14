278c19bc6c0a8655283d79fd77118d8b142544f9
babaliaysu <babali.aysu2007@gmail.com>
2026-02-14 00:26:55 +0000

    d

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
     total = 0
-    n = len(sys.argv) - 1
-    for i in range(1,n+1):
-        reqem = int(sys.argv[i])
-        total+=reqem
-    print("{:d}".format(total))
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
