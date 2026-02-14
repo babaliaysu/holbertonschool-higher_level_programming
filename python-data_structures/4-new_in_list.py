ca5cecf2384b6f6ac18244e3f61c06839bbc6f84
babaliaysu <babali.aysu2007@gmail.com>
2026-02-14 13:06:05 +0000

    K

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy_list = my_list[:]
+
+    if idx < 0:
+        return copy_list
+
+    if idx >= len(my_list):
+        return copy_list
+
+    copy_list[idx] = element
+    return copy_list
