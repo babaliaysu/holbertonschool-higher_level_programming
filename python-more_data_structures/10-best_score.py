c783d76379d30447141144297ece5c3e0f4b14ed
babaliaysu <babali.aysu2007@gmail.com>
2026-02-17 20:08:14 +0000

    K

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
 
     best_key = None
-    max_val = -1  
+    max_val = -1
 
     for key, value in a_dictionary.items():
         if best_key is None or value > max_val:
