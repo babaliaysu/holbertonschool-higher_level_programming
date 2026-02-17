93f317503652c36022a07e31ce0fdf935bf54e2e
babaliaysu <babali.aysu2007@gmail.com>
2026-02-17 20:06:41 +0000

    J'

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_key = None
+    max_val = -1  
+
+    for key, value in a_dictionary.items():
+        if best_key is None or value > max_val:
+            max_val = value
+            best_key = key
+
+    return best_key
