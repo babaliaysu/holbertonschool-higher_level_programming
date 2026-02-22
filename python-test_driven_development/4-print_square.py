d6813bea63897ff2e2741331a3861c6a0516503f
baabliaysu <babali.aysu2007@gmail.com>
2026-02-22 11:29:02 +0000

    k

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to print a square with #."""
+
+
+def print_square(size):
+    """Prints a square of a given size using #.
+
+    Args:
+        size (int): The side length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
