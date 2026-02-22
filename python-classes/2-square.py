2c8add100552b6b41ab2d06a6d722a045ee4343c
baabliaysu <babali.aysu2007@gmail.com>
2026-02-22 16:33:40 +0000

    L

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size.
+
+        Args:
+            size (int): The length of a side of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
