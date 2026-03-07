5999631cbd0249aaf5c8e044d9203cbd1cc5c6bd
babaliaysu <babali.aysu2007@gmail.com>
2026-03-07 19:49:35 +0000

    l

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,7 +7,7 @@ It provides a simple interface for file creation and overwriting.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of 
+    Writes a string to a text file (UTF8) and returns the number of
     characters written.
 
     Args:
