b87ee644ebe406db6a39494741d769863d5eceff
babaliaysu <babali.aysu2007@gmail.com>
2026-03-07 23:12:29 +0000

    l

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+
+class Student:
+    """Representation of a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary of the student"""
+        return self.__dict__
