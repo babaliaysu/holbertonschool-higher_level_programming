d556b04dc1982cbc05158ee42914c67e19f2dc9a
babaliaysu <babali.aysu2007@gmail.com>
2026-03-07 23:06:45 +0000

    l

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object"""
+    """Returns the dictionary description for JSON serialization"""
     return obj.__dict__
