9c096e099d1d3757ecaa871608e267bf01531983
babaliaysu <babali.aysu2007@gmail.com>
2026-03-15 07:11:13 +0000

    L

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table second_table.
+-- Results display both the score and the name.
+-- Records are ordered by score (top first).
+SELECT score, name FROM second_table ORDER BY score DESC;
