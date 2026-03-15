9ad59e26ce0140b7c3ba2760115940717638a18e
babaliaysu <babali.aysu2007@gmail.com>
2026-03-15 14:06:55 +0000

    L

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
-"""
-Displays all values in the states table where name matches the argument.
-Note: This script uses format() as REQUIRED by the task, but this is INSECURE
-and vulnerable to SQL injection attacks.
-"""
+"""Filter states by user input with basic SQL injection protection"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    # Create cursor
+    
     cur = db.cursor()
-
-    # TASK REQUIREMENT: Must use format() - THIS IS INSECURE!
-    # In production, NEVER do this. Use parameterized queries instead.
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cur.execute(query)
-
-    # Fetch and display results
-    results = cur.fetchall()
-    for row in results:
+    
+    # Təhlükəsizlik üçün dırnaqları escape edirik
+    state_name = sys.argv[4].replace("'", "\\'").replace('"', '\\"')
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
+    
+    for row in cur.fetchall():
         print(row)
-
-    # Clean up
+    
     cur.close()
     db.close()
