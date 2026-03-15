116c50f448fef90805ffb1a71c2ba930c86c32f8
babaliaysu <babali.aysu2007@gmail.com>
2026-03-15 07:01:12 +0000

    L

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current database.
+-- The script should not fail if the table already exists.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
