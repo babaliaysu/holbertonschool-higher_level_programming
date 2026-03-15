6c7fdde169271d4da54a6a4cbae263e66350e9a5
babaliaysu <babali.aysu2007@gmail.com>
2026-03-15 15:15:17 +0000

    L

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Deletes all State objects with a name containing the letter 'a'
-from the database hbtn_0e_6_usa.
 """
 import sys
 from sqlalchemy import create_engine
@@ -9,23 +8,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşulma
+    # Engine və Session-un düzgün qurulması
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Sessiya yaradılması
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Adında 'a' hərfi olan bütün ştatları tapırıq
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    # Silinəcək obyektləri tapırıq
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
-    # Hər birini siliirik
-    for state in states_to_delete:
+    # Obyektləri dövr ilə silirik
+    for state in states:
         session.delete(state)
 
-    # Dəyişikliyi bazaya tətbiq edirik
+    # Tranzaksiyanı bağlayırıq (və dəyişikliyi bazaya yazırıq)
     session.commit()
-
-    # Sessiyanın bağlanması
     session.close()
